36abb1ddb9bef1ebc64be206361f93b9085a64f6
anikulin <anikulin@emphasoft.com>
2022-10-14 11:01:31 +0300

    Refactor admin.py

--- reksoft/core/admin.py
+++ reksoft/core/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from core.models import Truck, TruckModel
 
 admin.site.register(TruckModel)
-admin.site.register(Truck)
+
+
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    search_fields = ["side_number__icontains"]
+    list_display = ["id", "side_number", "current_cargo_weight", "model"]
+    exclude = ["deleted_at"]
